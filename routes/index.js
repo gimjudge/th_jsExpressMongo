8d381acfebc1a5ab7d016a32875c3a3adf292fa2
gimjudge <gimjudge@hotmail.com>
2019-02-01 11:48:54 -0800

    Insert Mongo Users

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User =  require('../models/user')
 
 // GET /register
 router.get('/register', function(req, res, next) {
@@ -8,7 +9,43 @@ router.get('/register', function(req, res, next) {
 
 // POST /register
 router.post('/register', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+  if (
+    req.body.email &&
+    req.body.name &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword
+  ) {
+    // Confirm that user typed same password twice
+    if (req.body.password !== req.body.confirmPassword) {
+      var err = new Error('Passwords do not match.');
+      err.status = 400;
+      return next(err);
+    }
+
+    // Create object with form input
+    var userData = {
+      email: req.body.email,
+      name: req.body.name,
+      favoriteBook: req.body.favoriteBook,
+      password: req.body.password
+    };
+
+    // use schema's `create` method to insert document into Mongo
+    User.create(userData, function (error,user) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.redirect('/profile');
+      }
+    });
+
+
+  } else {
+      var err = new Error('All fields Required.');
+      err.status = 400;
+      return next(err);
+  }
 });
 
 // GET /
